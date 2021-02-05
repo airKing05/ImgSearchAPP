fedb7a1a8297de7fbe9ff54ef1cf2901f9165915
Anilraj <anil050797@gmail.com>
2021-02-05 23:33:53 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -3,29 +3,35 @@ import './App.css';
 
 function App() {
   const [value, setValue]=useState("");
-  const [results, setResults]=useState([]);
+  const [imgData, setImgData]=useState([]);
+
+  
   const fetchImg=()=>{
-    fetch(`https://api.unsplash.com/search/photos?client_id=YV024mO0UgSUAZdOAloXOygnslrl2I1xVjOA9Qeiacg&query=${value}&orientation=squarish&per_page=40`)
+    fetch(`https://api.unsplash.com/search/photos?client_id=YV024mO0UgSUAZdOAloXOygnslrl2I1xVjOA9Qeiacg&query=${value}&orientation=squarish&per_page=50`)
     .then((res)=>res.json())
     .then((data)=>{
       console.log(data)
-      setResults(data.results)})
+      setImgData(data.results)})
+    .catch(err=> {
+      console.log("there is an err", err) 
+    })
       
   };
   return (
     <div className="App">
-     <div className="main_div">
+     <div className="container">
        <input 
-       style={{width:"60%", padding:"10px"}}
+       style={{width:"60%", padding:"15px"}}
        type="text" 
        value={value} 
        onChange={(e)=>setValue(e.target.value)}  
        placeholder="Type your Requried Img"/>
        <button onClick={()=>fetchImg()}>Search</button>
      </div>
+
      <div className="img_div">
        {
-         results.map((item)=>{
+         imgData && imgData.map((item)=>{
            return <img key={item.id} src={item.urls.regular}/>
          })
        }
